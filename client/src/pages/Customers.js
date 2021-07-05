2b2f1b9d830ea4abb1f3a84acd43912f2ab76ba4
Laura Aupert <aupert.laura@gmail.com>
2021-07-05 18:02:08 -0400

    Page for displaying customer list and search

---
+++ client/src/pages/Customers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CustomerTable from '../components/CustomerTable';
+
+function Customers() {
+    return ( 
+        <CustomerTable />
+    )
+}
+
+export default Customers;
